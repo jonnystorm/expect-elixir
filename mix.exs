79f522dfc30f54fb503a848a892559129c206bc3
Billy Forester <william.forester@gmail.com>
2018-10-13 13:12:58 -0500

    porcelain2.ex needed to have a module name change to not conflict with porcelain.ex

--- mix.exs
+++ mix.exs
@@ -2,28 +2,29 @@ defmodule Expect.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :expect_ex,
+    [
+      app: :expect_ex,
       version: "0.0.4",
       name: "expect-elixir",
       source_url: "https://gitlab.com/jonnystorm/expect-elixir",
       elixir: "~> 1.3",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [extras: ["README.md"]],
       dialyzer: [
         add_plt_apps: [
           :logger,
-          :porcelain,
+          :porcelain
         ],
         ignore_warnings: "dialyzer.ignore",
         flags: [
           :unmatched_returns,
           :error_handling,
           :race_conditions,
-          :underspecs,
-        ],
-      ],
+          :underspecs
+        ]
+      ]
     ]
   end
 
@@ -34,20 +35,20 @@ defmodule Expect.Mixfile do
     do: [driver: Expect.Driver.Porcelain]
 
   def application do
-    [ applications: [
+    [
+      applications: [
         :logger,
         :porcelain
       ],
-      env: Keyword.merge([], get_env(Mix.env))
+      env: Keyword.merge([], get_env(Mix.env()))
     ]
   end
 
   defp deps do
-    [ { :porcelain,
-        git: "https://github.com/alco/porcelain",
-        ref: "acfc0f0a6987aadb08f495a578e22ef624342685"
-      },
-      {:ex_doc, "~> 0.13", only: :dev},
+    [
+      {:porcelain,
+       git: "https://github.com/alco/porcelain", ref: "acfc0f0a6987aadb08f495a578e22ef624342685"},
+      {:ex_doc, "~> 0.13", only: :dev}
     ]
   end
 end
